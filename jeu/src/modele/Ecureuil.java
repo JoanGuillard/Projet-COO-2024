5130da911a6efa614a162c019ce3f9e74ea90089
Jojoskooo <joan.guillard@gmail.com>
2024-12-08 20:48:56 +0100

    initialisation de la branche test

--- jeu/src/modele/Ecureuil.java
+++ jeu/src/modele/Ecureuil.java
@@ -5,7 +5,7 @@ public class Ecureuil extends Animal{
 
     public Ecureuil(int abscisse, int ordonnee, int nbNourritureAmi, int nbTourSansManger) {
         super(abscisse, ordonnee, nbNourritureAmi, nbTourSansManger);
-        setApparence("E" );
+        setApparence("E");
         this.getRegimeAlimentaire().add("C");
         this.getRegimeAlimentaire().add("G");
 
